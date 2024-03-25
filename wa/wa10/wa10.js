be064239e359ec8a17c5f9c9ed808668029a0fc3
Peterb15 <pebu5033@colorado.edu>
2024-03-24 22:58:19 -0600

    Weekly Assignment 10 - Silly Story Generator
    Created a Silly Story Generator that would take in a custom name input and created a story based on a string of randomly generated names, place, and events. Using JS to complete, and CSS to style the code

--- wa/wa10/wa10.js
+++ wa/wa10/wa10.js
@@ -24,12 +24,13 @@ spontaneously combusted
 melted into a puddle on the sidewalk
 turned into a slug and crawled away
 */
-const storyText = ' It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const storyText = 'The world was ripe with adventure and treasure so :insertx: and Pete took to the seas. They traveled across the seas until they reached :inserty:. Here they found the adventure they had seeked, then :insertx: :insertz:. Pete continued to sail across the sea, the final treasure of Pete and :insertx: weighed in at 2000 pounds (and strangely it was also noted that it was 94 fahrenheit on the day their voyage set sail).';
+const insertX = ['Atlas the Pirate', 'Illvana', 'Quinn the Cowboy'];
+const insertY = ['the Soros Isles', 'the Last Bastion', 'the Wastes'];
+const insertZ = ['saw god and ascended', 'was reduced to ash', 'lived a life of adventure and excitement'];
 
 // 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION 
+
 
 randomize.addEventListener('click', result);
 
@@ -44,16 +45,16 @@ function result() {
 
     if(customName.value !== '') {
         const name = customName.value;
-        newStory = newStory.replace('Bob', name);
+        newStory = newStory.replaceAll('Pete', name);
 
     }
 
 
     if(document.getElementById("uk").checked) {
-        const weight = Math.round(300/14) + ' stones ';
+        const weight = Math.round(2000/14) + ' stones ';
         const temperature =  Math.round((94 - 32)*(5/9)) + ' centigrade ';
 
-        newStory = newStory.replace('300 pounds', weight);
+        newStory = newStory.replace('2000 pounds', weight);
         newStory = newStory.replace('94 fahrenheit', temperature);
 
     }
